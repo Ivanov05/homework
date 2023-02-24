229e70c8fff7f5355a9b5d6387d0876a8670aced
Ivanov05 <ahil71963@gmail.com>
2023-02-24 10:11:03 +0200

    Add project files.

---
+++ homework/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wensday":
+                case "Thursday":
+                case "Friday":
+                    Console.WriteLine("Warking day");
+                    break;
+                case "Saturday":
+                case "Sunday":
+                    Console.WriteLine("Weekemd");
+                    break ;
+                default:
+                    Console.WriteLine("Error");
+                    break;
+            }
+        }
+    }
+}
